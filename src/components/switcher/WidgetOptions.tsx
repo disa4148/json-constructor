ac39b1363ff05e3bc84336ac7dc7dbdc688fa34b
Rodion <79634559+PurpleTooth@users.noreply.github.com>
2023-11-29 19:28:35 +0700

    Major changes to the application logic

--- src/components/switcher/WidgetOptions.tsx
+++ src/components/switcher/WidgetOptions.tsx
@@ -1,105 +1,116 @@
-import { useState, useEffect } from "react"
-import JsonDisplay from "../JsonDisplay"
+import React, { useState, useEffect } from "react";
+import JsonDisplay from "../JsonDisplay";
 
 interface OptionProps {
-   text?: string
-   color?: string
-   fontSize?: number
-   fontWeight?: number
-   fontStyle?: string
-   verticalAlign?: string
-   horizontalAlign?: string
-   blockHeight?: number
-   blockWidth?: number
-   marginRight?: number
-   digitsAfterDot?: number
-   [key: string]: any
+  text?: string;
+  color?: string;
+  fontSize?: number;
+  fontWeight?: number;
+  fontStyle?: string;
+  verticalAlign?: string;
+  horizontalAlign?: string;
+  blockHeight?: number;
+  blockWidth?: number;
+  marginRight?: number;
+  digitsAfterDot?: number;
+  [key: string]: any;
 }
 
 interface WidgetOptionProps {
    data: {
-      displayedName: string
-      options: OptionProps
-   }
-}
-
-const WidgetOptions: React.FC<WidgetOptionProps> = ({
-   data: { displayedName, options },
-}) => {
-   const getInputType = (value: any) =>
-      typeof value === "number" ? "number" : "text"
-
-   const [inputValues, setInputValues] = useState<Record<string, any>>({})
-
+     displayedName: string;
+     widget: {
+       type: string;
+       options: OptionProps;
+     };
+     id: string;
+     x: number;
+     y: number;
+     width: number;
+     height: number;
+   };
+ }
+ 
+ const WidgetOptions: React.FC<WidgetOptionProps> = ({
+   data: { displayedName, widget, id, x, y, width, height },
+ }) => {
+   const getInputType = (value: any) => (typeof value === "number" ? "number" : "text");
+ 
+   const [inputValues, setInputValues] = useState<Record<string, any>>({});
+   const [localInputValues, setLocalInputValues] = useState<Record<string, any>>({});
+ 
    useEffect(() => {
-      const initialValues: Record<string, any> = {}
-      const fillInitialValues = (obj: any, prefix = "") => {
-         Object.keys(obj).forEach(key => {
-            const value = obj[key]
-            const fullKey = prefix ? `${prefix}.${key}` : key
-            if (value && typeof value === "object" && !Array.isArray(value)) {
-               fillInitialValues(value, fullKey)
-            } else {
-               initialValues[fullKey] = value ?? ""
-            }
-         })
-      }
-      fillInitialValues(options)
-      setInputValues(JSON.parse(JSON.stringify(options)));
-  }, [options]);
-
-  const handleInputChange = (path: string, value: any) => {
-   setInputValues(prevValues => {
-     // Создаем копию текущего состояния
-     const newValues = JSON.parse(JSON.stringify(prevValues));
-
-     // Получаем доступ к последнему элементу в пути
-     const keys = path.split('.');
-     let current = newValues;
-     keys.forEach((key, index) => {
-       if (index === keys.length - 1) {
-         current[key] = value;
+     const initialValues = {
+       displayedName, 
+       id, 
+       x, 
+       y, 
+       width, 
+       height, 
+       widget
+     };
+     setInputValues(initialValues);
+     setLocalInputValues(initialValues);
+   }, [displayedName, widget, id, x, y, width, height]);
+ 
+   const handleInputChange = (path: string, value: any) => {
+     setLocalInputValues(prev => ({...prev, [path]: value}));
+     // Обновляем глобальное состояние, чтобы сохранить структуру данных
+     setInputValues(prevValues => {
+       const keys = path.split('.');
+       const newValues = { ...prevValues };
+       let current = newValues;
+       for (let i = 0; i < keys.length - 1; i++) {
+         if (!current[keys[i]]) {
+           current[keys[i]] = {};
+         }
+         current = current[keys[i]];
+       }
+       current[keys[keys.length - 1]] = value;
+       return newValues;
+     });
+   };
+ 
+   const renderFields = (data: any, parentPath = "") => {
+     return Object.entries(data).map(([key, value]) => {
+       const path = parentPath ? `${parentPath}.${key}` : key;
+ 
+       if (typeof value === 'object' && value !== null) {
+         return (
+           <div key={path}>
+             <fieldset>
+               <legend>{key}</legend>
+               {renderFields(value, path)}
+             </fieldset>
+           </div>
+         );
        } else {
-         current = current[key];
+         return (
+           <div key={path}>
+             <label>{key}: </label>
+             <input
+               type={getInputType(value)}
+               value={localInputValues[path]}
+               onChange={(e) => handleInputChange(path, e.target.value)}
+             />
+           </div>
+         );
        }
      });
-
-     return newValues;
-   });
- };
-
- const renderFields = (data: any, parentKey = ''):React.ReactNode => {
-   return Object.entries(data).map(([key, value]) => {
-     const fieldKey = parentKey ? `${parentKey}.${key}` : key;
-     if (typeof value === 'object' && value !== null) {
-       return renderFields(value, fieldKey);
-     } else {
-       return (
-         <div key={fieldKey}>
-           <label>{fieldKey}: </label>
-           <input
-             type={getInputType(value)}
-             value={inputValues[fieldKey] ?? ''}
-             onChange={(e) => handleInputChange(fieldKey, e.target.value)}
-           />
-         </div>
-       );
-     }
-   });
- };
-
+   };
+ 
    return (
-      <div>
-         <h1>{displayedName}</h1>
-         <div className='flex flex-col gap-4'>
+     <div>
+       <h1>{displayedName}</h1>
+       <div className="flex flex-col gap-4">
          {renderFields(inputValues)}
-         </div>
-         <button className='mt-[20px]' onClick={() => console.log(inputValues)}>
-            Добавить
-         </button>
-         <JsonDisplay data={inputValues} />
-      </div>
-   )
-}
-
-export default WidgetOptions
+       </div>
+       <button className="mt-[20px]" onClick={() => console.log(inputValues)}>
+         Сохранить
+       </button>
+       <JsonDisplay data={inputValues} />
+     </div>
+   );
+ };
+ 
+ export default WidgetOptions;
--- src/constants/SelectorList.ts
+++ src/constants/SelectorList.ts
@@ -2,11 +2,24 @@ export const selectorList = [
    {
       displayedName: "Логотип РСДУ",
       type: "Labels.Rsdu5Logo",
-      options: {},
+      id: "W010",
+      x: 0,
+      y: 2,
+      width: 1,
+      height: 1,
+      widget: {
+      type: "Labels.Rsdu5Logo",
+      options: {}
+      }
    },
    {
       displayedName: "Дата и время",
       type: "Labels.DataTime",
+      id: "DATETIME",
+      x: 1704,
+      y: 0,
+      width: 200,
+      height: 2,
       options: {
          format: "DD.MM.YYYY HH:mm:ss",
          color: "#0000ff",
@@ -17,74 +30,316 @@ export const selectorList = [
       },
    },
    {
+      
       displayedName: "Текст",
-      type: "Labels.SimpleText",
-      options: {
-         text: "ПС КВАРЦ 110 кВ",
-         color: "#0000ff",
-         fontSize: 30,
-         fontWeight: 600,
-         fontStyle: "normal",
-         textAlign: "justify",
-      },
-   },
-   {
-      displayedName: "Телеизмерение",
-      type: "Analogs.SingleValue",
-      options: {
-         title: {
-            text: "P факт",
-            color: "#ff0000",
-            fontSize: 25,
-            fontWeigh: 400,
-            fontStyle: "normal",
-            verticalAlign: "center",
-            horizontalAlign: "left",
-         },
-         value: {
-            digitsAfterDot: 3,
-            color: "#00ff00",
-            fontSize: 30,
-            fontWeight: 800,
-            fontStyle: "italic",
-            verticalAlign: "center",
-            horizontalAlign: "center",
-            blockHeight: 70,
-            blockWidht: 70,
-            marginRight: 10,
-         },
-         unit: {
-            text: "МВт",
+      id: "W006",
+      x: 0,
+      y: 2,
+      width: 1,
+      height: 2,
+     
+      widget: {
+         type: "Labels.SimpleText",
+         options: {
+            text: "ПС КВАРЦ 110 кВ",
             color: "#0000ff",
             fontSize: 30,
-            fontWeight: 800,
+            fontWeight: 600,
             fontStyle: "normal",
-            verticalAlign: "center",
-            horizontalAlign: "right",
+            textAlign: "justify",
+         },
+   },
+},
+   {
+      displayedName: "Телеизмерение",
+      id: "W006",
+      x: 0,
+      y: 2,
+      width: 1,
+      height: 2,
+     
+      widget: {
+         type: "Analogs.SingleValue",
+         options: {
+            title: {
+               text: "P факт",
+               color: "#ff0000",
+               fontSize: 25,
+               fontWeigh: 400,
+               fontStyle: "normal",
+               verticalAlign: "center",
+               horizontalAlign: "left",
+            },
+            value: {
+               digitsAfterDot: 3,
+               color: "#00ff00",
+               fontSize: 30,
+               fontWeight: 800,
+               fontStyle: "italic",
+               verticalAlign: "center",
+               horizontalAlign: "center",
+               blockHeight: 70,
+               blockWidht: 70,
+               marginRight: 10,
+            },
+            unit: {
+               text: "МВт",
+               color: "#0000ff",
+               fontSize: 30,
+               fontWeight: 800,
+               fontStyle: "normal",
+               verticalAlign: "center",
+               horizontalAlign: "right",
+            },
+            rtdata: [
+               {
+                  tag: "",
+                  tableId: 29,
+                  paramId: 5010404,
+                  gtopt: "",
+               },
+            ],
+         },
+      }
+      
+   },
+   {
+      displayedName: "Таблица",
+      type: "Analogs.TableGtp",
+      id: "W008",
+      x: 2,
+      y: 2,
+      width: 1,
+      height: 2,
+      widget: {
+         type: "Analogs.TableGtp",
+         options: {
+            table: {
+               headerHeight: 30,
+               columns: [
+                  {
+                     width: 20
+                  },
+                  {
+                     width: 40,
+                     header: {
+                        label: "P,МВт",
+                        fontSize: 16,
+                        fontWeight: 400
+                     },
+                     body: {
+                        countNumbersAfterDot: 3,
+                        fontSize: 16,
+                        fontWeight: 600
+                     }
+                  },
+                  {
+                     width: 40,
+                     header: {
+                        label: "Q, МВАр",
+                        fontSize: 16,
+                        fontWeight: 400
+                     },
+                     body: {
+                        countNumbersAfterDot: 1,
+                        fontSize: 16,
+                        fontWeight: 600
+                     }
+                  }
+               ],
+               rows: [
+                  {
+                     columnsData: [
+                        {
+                           label: "ТГ-2"
+                        },
+                        {
+                           tag: "pTg2"
+                        },
+                        {
+                           tag: "qTg2"
+                        }
+                     ]
+                  },
+                  {
+                     columnsData: [
+                        {
+                           label: "ТГ-3"
+                        },
+                        {
+                           tag: "pTg3"
+                        },
+                        {
+                           tag: "qTg3"
+                        }
+                     ]
+                  }
+               ]
+            }
          },
          rtdata: [
             {
-               tag: "",
-               tableId: 29,
-               paramId: 5010404,
-               gtopt: "",
+               tag: "pTg2",
+               tableId: 1320,
+               paramId: 11,
+               gtopt: ""
             },
-         ],
-      },
+            {
+               tag: "qTg2",
+               tableId: 1320,
+               paramId: 163,
+               gtopt: ""
+            },
+            {
+               tag: "pTg3",
+               tableId: 1320,
+               paramId: 12,
+               gtopt: ""
+            },
+            {
+               tag: "qTg3",
+               tableId: 1320,
+               paramId: 164,
+               gtopt: ""
+            }
+         ]
+      }
    },
-   { displayedName: "Таблица", type: "Analogs.TableGtp", options: {
-    
-   } },
+
    {
       displayedName: "Действующая команда",
       type: "Labels.LastCommand",
-      options: {},
+      id: "W004",
+      x: 0,
+      y: 3,
+      width: 1,
+      height: 2,
+      widget: {
+         type: "Labels.LastCommand",
+         options: {
+            gtpId: 3,
+            technologyId: "P",
+            initiativeFontSize: 25,
+            initiativeColor: "#FF0000",
+            titleFontSize: 25,
+            titleColor: "#00FF00",
+            receivedDateFontSize: 12,
+            receivedDateColor: "#0000FF",
+            receivedTimeFontSize: 14,
+            receivedTimeColor: "#0000FF",
+            completedTimeFontSize: 20,
+            completedTimeColor: "#0000FF"
+         }
+      }
    },
    {
       displayedName: "График активной мощности ГОУ",
       type: "Charts.ChartGtp",
-      options: {},
+      id: "W013",
+      x: 0,
+      y: 4,
+      width: 2,
+      height: 6,
+      widget: {
+         type: "Charts.ChartGtp",
+         options: {
+            text: "P факт",
+            interval: "day",
+            intervalBefore: 60,
+            intervalAfter: 60
+         },
+         archives: [
+            {
+               tag: "pmin",
+               tableId: 1320,
+               paramId: 220,
+               gtopt: "GLT_ANALOG_OPT_CUR1"
+            },
+            {
+               tag: "pmax",
+               tableId: 1320,
+               paramId: 221,
+               gtopt: "GLT_ANALOG_OPT_CUR1"
+            },
+            {
+               tag: "ddg",
+               tableId: 1320,
+               paramId: 219,
+               gtopt: "GLT_ANALOG_OPT_CUR1"
+            },
+            {
+               tag: "udg",
+               tableId: 1320,
+               paramId: 222,
+               gtopt: "GLT_ANALOG_OPT_CUR1"
+            },
+            {
+               tag: "p",
+               tableId: 1320,
+               paramId: 144,
+               gtopt: "GLT_ANALOG_OPT_MEAN1"
+            }
+         ],
+         rtdata: [
+            {
+               tag: "p",
+               tableId: 1320,
+               paramId: 144,
+               gtopt: ""
+            }
+         ]
+      }
    },
-   { displayedName: "График", type: "Charts.SimpleChart", options: {} },
+   {
+      displayedName: "График",
+      type: "Charts.SimpleChart",
+      id: "W0011",
+      x: 1,
+      y: 1,
+      width: 1,
+      height: 4,
+      widget: {
+         type: "Charts.SimpleChart",
+         options: {
+            text: "P генераторов, МВт",
+            interval: "day",
+            intervalBefore: 135,
+            intervalAfter: 135,
+            legend: {
+               show: true,
+               position: "top"
+            },
+            yaxis: {
+               axisName: "МВт"
+            },
+            seriesOptions: {
+               ptg2: {
+                  name: "P ТГ2",
+                  lineColor: "blue",
+                  lineWidth: 1
+               },
+               ptg4: {
+                  name: "P ТГ4",
+                  lineColor: "purple",
+                  lineWidth: 1
+               }
+            }
+         },
+         archives: [
+            {
+               tag: "ptg2",
+               tableId: 1320,
+               paramId: 11,
+               gtopt: "GLT_ANALOG_OPT_MEAN1"
+            },
+            {
+               tag: "ptg4",
+               tableId: 1320,
+               paramId: 13,
+               gtopt: "GLT_ANALOG_OPT_MEAN1"
+            }
+         ]
+      }
+   },
 ]
 export default selectorList
